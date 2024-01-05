83c9fa024c91060fa9ce1e3c0f55f48391d6884e
陳進旺 <111399570+Cjw0523@users.noreply.github.com>
2024-01-05 13:27:41 +0800

    Add files via upload

---
+++ demo/src/test/java/com/example/demo/CalculatorTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+
+    @Disabled
+    @Test
+    public void add() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(1,2);
+
+        assertNotNull(result);
+        assertEquals(3, result);
+        assertTrue(result > 1);
+    }
+
+    @DisplayName("測試除法問題")
+    @Test
+    public void divide() {
+        Calculator calculator = new Calculator();
+
+        assertThrows(ArithmeticException.class, () -> {
+            calculator.divide(1, 0);
+        });
+    }
+}
